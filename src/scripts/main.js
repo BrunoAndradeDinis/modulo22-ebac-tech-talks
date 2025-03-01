680ed5d6e7f23ee8885d9d9726fbaf88868e02d0
Bruni <bruno.a.dinis96@gmail.com>
2025-03-01 17:47:16 -0300

    feat: criado a contagem regressiva, adicionado a interação através do javascript.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(() => {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const diasEmMs = 1000 * 60 * 60 * 24;
+  const horasEmMs = 1000 * 60 * 60;
+  const minutosEmMs = 1000 * 60;
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diasEmMs) / horasEmMs
+  );
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % horasEmMs) / minutosEmMs
+  );
+  const segundosAteOEvento = Math.floor(
+    (distanciaAteOEvento % minutosEmMs) / 1000
+  );
+
+  if (diasAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("contador").innerHTML = "Evento expirado! 😥";
+  } else {
+    document.getElementById(
+      "contador"
+    ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+  }
+}, 1000);
